2862a6a61e50bbbd41791961e1e38807186fe482
Владимир Колпаков <vix-sp@yandex.ru>
2023-05-14 23:01:56 +0300

    Add model GenreTitle.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -10,12 +10,12 @@ class Category(models.Model):
     # slug - required, string <= 50 characters ^[-a-zA-Z0-9_]+$
     slug = models.SlugField('Slug категории')
 
-    def __str__(self):
-        return self.name
-
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
 
 
 class Genre(models.Model):
@@ -24,13 +24,21 @@ class Genre(models.Model):
     # slug - required, string <= 50 characters ^[-a-zA-Z0-9_]+$
     slug = models.SlugField('Slug жанра')
 
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
 
 class Title(models.Model):
     # Получение списка всех произведений, к которым пишут отзывы.
     name = models.CharField('Название', max_length=256)
     year = models.IntegerField('Год выпуска')
     description = models.CharField('Описание', max_length=256)
-    genre = models.SlugField('Жанр')
+    # genre = models.SlugField('Жанр')
+    genre = models.ManyToManyField(Genre, through='GenreTitle')
     category = models.ForeignKey(
         Category,
         # Доступ ко всем произведениям определенной категории
@@ -41,8 +49,34 @@ class Title(models.Model):
         # При удалении категории, произведения сохранятся
         on_delete=models.SET_NULL,
         verbose_name='Категория',
-        help_text='Категория, к которой будет относиться произведение'
+        help_text='Категория, к которой будет относиться произведение',
     )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
 
     def __str__(self):
         return self.name
+
+
+class GenreTitle(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = 'Жанр произведения'
+        verbose_name_plural = 'Жанры произведения'
+
+    def __str__(self):
+        return f'{self.genre} {self.title}'

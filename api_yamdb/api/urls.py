a28a91d81587cff399aa931110d6146b6837ef3b
Владимир Колпаков <vix-sp@yandex.ru>
2023-05-15 14:44:34 +0300

    Add routers.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -5,11 +5,10 @@ from .views import TitleViewSet, CategoryViewSet, GenreViewSet
 
 
 router_v1 = DefaultRouter()
-router_v1.register(r'titles', TitleViewSet, basename='title')
+router_v1.register(r'titles', TitleViewSet)
 router_v1.register(r'categories', CategoryViewSet)
 router_v1.register(r'genres', GenreViewSet)
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
-
 ]

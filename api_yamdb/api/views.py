1f15e6aec38521b39dbeaf7e61ea49b6e6214d76
Владимир Колпаков <vix-sp@yandex.ru>
2023-05-15 14:45:08 +0300

    Add viewsets.

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,13 +1,19 @@
 from rest_framework import viewsets
 
+from reviews.models import Category, Genre, Title
+from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
+
 
 class TitleViewSet(viewsets.ModelViewSet):
-    pass
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
-    pass
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
 
 class GenreViewSet(viewsets.ModelViewSet):
-    pass
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer

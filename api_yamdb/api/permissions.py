96cb01d8c7f095394de8bfb5211c30432ebd63d0
Никита Зивенко <flyingfry@yandex.ru>
2023-05-16 12:00:29 +0300

    Add user roles check

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -13,6 +13,7 @@ class AuthorOrStaffEditPermission(permissions.BasePermission):
         return (
             request.method in permissions.SAFE_METHODS
             or obj.author == request.user
-            # or request.user.role == moderator
-            # or request.user.role == admin
+            or request.user.role == 'moderator'
+            or request.user.role == 'admin'
+            or request.user.is_superuser
         )

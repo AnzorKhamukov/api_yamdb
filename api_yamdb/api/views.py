99dcdf1e6422786c75a8fa11bbdebd3d787ba728
Никита Зивенко <flyingfry@yandex.ru>
2023-05-17 11:05:43 +0300

    Add LimitOffsetPagination to viewsets

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.pagination import LimitOffsetPagination
 
 from reviews.models import Review, Title
 from .permissions import AuthorOrStaffEditPermission
@@ -9,6 +10,7 @@ from .serializers import CommentSerializer, ReviewSerializer
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (AuthorOrStaffEditPermission,)
+    pagination_class = LimitOffsetPagination
 
     def get_queryset(self):
         review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
@@ -22,6 +24,7 @@ class CommentViewSet(viewsets.ModelViewSet):
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
     permission_classes = (AuthorOrStaffEditPermission,)
+    pagination_class = LimitOffsetPagination
 
     def get_queryset(self):
         title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))

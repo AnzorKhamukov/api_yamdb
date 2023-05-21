85f806891ece55180c95167cdeb94ae96a27de60
Владимир Колпаков <vix-sp@yandex.ru>
2023-05-21 09:53:01 +0300

    Add basename.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -17,9 +17,9 @@ v1_router.register(
     basename='comments',
 )
 
-v1_router.register(r'titles', TitleViewSet)
-v1_router.register(r'categories', CategoryViewSet)
-v1_router.register(r'genres', GenreViewSet)
+v1_router.register(r'titles', TitleViewSet, basename='titles')
+v1_router.register(r'categories', CategoryViewSet, basename='categories')
+v1_router.register(r'genres', GenreViewSet, basename='genres')
 v1_router.register(r'users', UserViewSet, basename='users')
 
 

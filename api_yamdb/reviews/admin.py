41d353cada5b7220c2f28035cf00c07ec8c0c4d0
Владимир Колпаков <vix-sp@yandex.ru>
2023-05-21 09:50:21 +0300

    Add GenreAdmin, CategoryAdmin, TitleAdmin.

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -3,9 +3,28 @@ from django.contrib import admin
 from .models import Category, Genre, Title
 from .models import Review, Comment, User
 
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug',)
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug',)
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'year', 'category',)
+    search_fields = ('name', 'year')
+    empty_value_display = '-пусто-'
+
+
 admin.site.register(Review)
 admin.site.register(Comment)
-admin.site.register(Category)
-admin.site.register(Genre)
-admin.site.register(Title)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Title, TitleAdmin)
 admin.site.register(User)
